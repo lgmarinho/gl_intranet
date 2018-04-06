e2ac9af6f2d8254768af82ac8e7a4f2bbfcbe850
luizgustavo <luizgustavo@gaylussac.com.br>
2018-04-06 16:02:45 -0300

    Adicionando arquivos

---
+++ teste.php
@@ -0,0 +1,7 @@
+<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Usuários<span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a>
+          <ul class="dropdown-menu forAnimate" role="menu">
+            <li><a href="#">Incluir Usuário</a></li>
+            <li><a href="#">Alterar Usuário</a></li>           
+          </ul>
+        </li> 
